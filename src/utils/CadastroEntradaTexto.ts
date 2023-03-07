f444d2f741d8109867e827658f92d2c85cb7710b
andreocunha <andreoliveiracunha20@gmail.com>
2023-03-07 16:03:58 -0300

    aula 3.4 - desafio

--- src/utils/CadastroEntradaTexto.ts
+++ src/utils/CadastroEntradaTexto.ts
@@ -12,6 +12,18 @@ const secoes = [
         id: 2,
         label: 'Email',
         placeholder: 'Digite seu email'
+      },
+      {
+        id: 3,
+        label: 'Crie uma senha',
+        placeholder: 'Insira sua senha',
+        secureTextEntry: true,
+      },
+      {
+        id: 4,
+        label: 'Confirme sua senha',
+        placeholder: 'Insira sua senha',
+        secureTextEntry: true,
       }
     ],
     checkbox: []
@@ -23,8 +35,28 @@ const secoes = [
       {
         id: 1,
         label: 'CEP',
-        placeholder: 'Digite seu CEP'
-      }
+        placeholder: 'Insira seu CEP'
+      },
+      {
+        id: 2,
+        label: 'Endereço',
+        placeholder: 'Insira seu endereço'
+      },
+      {
+        id: 3,
+        label: 'Número',
+        placeholder: 'Insira seu número'
+      },
+      {
+        id: 4,
+        label: 'Complemento',
+        placeholder: 'Insira seu complemento'
+      },
+      {
+        id: 5,
+        label: 'Telefone',
+        placeholder: '(00) 00000-0000'
+      },
     ],
     checkbox: []
   },
@@ -40,6 +72,30 @@ const secoes = [
       {
         id: 2,
         value: 'Unimed'
+      },
+      {
+        id: 3,
+        value: 'Bradesco'
+      },
+      {
+        id: 4,
+        value: 'Amil'
+      },
+      {
+        id: 5,
+        value: 'Biosaúde'
+      },
+      {
+        id: 6,
+        value: 'Biovida'
+      },
+      {
+        id: 7,
+        value: 'Outros'
+      },
+      {
+        id: 8,
+        value: 'Não tenho plano'
       }
     ]
   }

7e2eb982a18c30c36d83ba6a15356253c08bcc3a
andreocunha <andreoliveiracunha20@gmail.com>
2023-03-08 15:26:05 -0300

    aula 5.1

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,9 +1,25 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base'
+import { Titulo } from '../componentes/Titulo'
 
 export default function Perfil(){
   return(
-    <VStack>
-      <Text>Perfil</Text>
-    </VStack>
+    <ScrollView flex={1}>
+      <VStack flex={1} alignItems="center" p={5}>
+        <Titulo color="blue.500">Meu Perfil</Titulo>
+
+        <Avatar size="xl" source={{ uri: "https://github.com/andreocunha.png" }} mt={5} />
+
+        <Titulo color="blue.500">Informações pessoais</Titulo>
+        <Titulo fontSize="lg" mb={1}>André Cunha</Titulo>
+        <Text>12/12/1990</Text>
+        <Text>São Paulo</Text>
+
+        <Divider mt={5} />
+
+        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+      </VStack>
+    </ScrollView>
   )
 }

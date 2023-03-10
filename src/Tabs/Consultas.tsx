d1f08c072d328e9b34c64aaf8b33d89af87d2028
andreocunha <andreoliveiracunha20@gmail.com>
2023-03-10 15:34:16 -0300

    aula 5.2

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,9 +1,16 @@
 import { VStack, Text } from 'native-base'
+import { CardConsulta } from '../componentes/CardConsulta'
 
 export default function Consultas(){
   return(
-    <VStack>
+    <VStack p="5">
       <Text>Consultas</Text>
+      <CardConsulta 
+        nome='Dr. Andre'
+        especialidade='Cardiologista'
+        foto='https://github.com/andreocunha.png'
+        data='20/04/2023'
+      />
     </VStack>
   )
 }
---
+++ src/componentes/CardConsulta.tsx
@@ -0,0 +1,36 @@
+import { Text, Avatar, VStack } from 'native-base'
+import { Botao } from './Botao'
+
+interface CardProps {
+  nome: string;
+  foto: string;
+  especialidade: string;
+  data?: string;
+  foiAtendido?: boolean;
+  foiAgendado?: boolean;
+}
+
+export function CardConsulta({
+  nome,
+  foto, 
+  data,
+  especialidade,
+  foiAgendado,
+  foiAtendido
+}: CardProps){
+  return(
+    <VStack w="100%" bg={foiAtendido ? 'blue.100': 'white'} p="5" borderRadius="lg" shadow="2">
+      <VStack flexDir="row">
+        <Avatar size="lg" source={{ uri: foto }} />
+        <VStack pl="4">
+          <Text fontSize="md" bold>{nome}</Text>
+          <Text>{especialidade}</Text>
+          <Text>{data}</Text>
+        </VStack>
+      </VStack>
+      <Botao mt={4}>
+        {foiAgendado ? 'Cancelar consulta' : 'Agendar consulta'}
+      </Botao>
+    </VStack>
+  )
+}

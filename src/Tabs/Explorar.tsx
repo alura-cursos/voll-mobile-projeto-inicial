2c561a455bdafa06b074c0a302084960cd0462a0
andreocunha <andreoliveiracunha20@gmail.com>
2023-03-10 15:53:54 -0300

    tela explorar - desafio

--- src/Tabs/Explorar.tsx
+++ src/Tabs/Explorar.tsx
@@ -1,9 +1,36 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Box, ScrollView } from "native-base";
+import { Botao } from "../componentes/Botao";
+import { CardConsulta } from "../componentes/CardConsulta";
+import { EntradaTexto } from "../componentes/EntradaTexto";
+import { Titulo } from "../componentes/Titulo";
 
 export default function Explorar(){
   return(
-    <VStack>
-      <Text>Explorar</Text>
-    </VStack>
+    <ScrollView flex={1} bgColor="white">
+      <VStack flex={1} alignItems="flex-start" justifyContent="flex-start" p={5}>
+        <Box w="100%" borderRadius="lg" p={3} mt={5} shadow="1" borderRightRadius="md">
+          <EntradaTexto
+            placeholder="Digite a especialidade"
+          />
+          <EntradaTexto
+            placeholder="Digite sua localização"
+          />
+          <Botao mt={3} mb={3}>
+            Buscar
+          </Botao>
+        </Box>
+
+        <Titulo color="blue.500" alignSelf="center">Resultado da Busca</Titulo>
+        {[1, 2, 3].map((_, index) => (
+          <VStack flex={1} w="100%" alignItems="flex-start" bgColor="white" key={index}>
+            <CardConsulta 
+              especialidade="Cardiologia"
+              foto="https://github.com/andreocunha.png"
+              nome="Dr. Cunha"
+            />
+          </VStack>
+        ))}
+      </VStack>
+    </ScrollView>
   )
 }
